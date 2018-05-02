089e9f6f5b729b1884a4207f3260b524db0b7b5c
Léo DESIGAUX <leodesigaux@gmail.com>
2018-05-02 19:32:23 +0200

    Added JS testing

--- app/index.js
+++ app/index.js
@@ -8,3 +8,16 @@ import 'styles/index.scss';
 // ================================
 // START YOUR APP HERE
 // ================================
+window.onload = () => {
+
+  let logo = document.querySelector('#logo')
+  let container = document.querySelector('.container')
+
+  logo.addEventListener('click', evt => {
+    evt.preventDefault()
+    console.log('detest')
+    container.classList.remove('container-contact')
+    container.classList.add('container-frontpage')
+    console.log('container.classList', container.classList)
+  })
+}
