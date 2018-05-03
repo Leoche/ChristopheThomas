2cc132baef3286bb88e55ed733265dd89ea1c16c
Léo DESIGAUX <leodesigaux@gmail.com>
2018-05-03 03:10:45 +0200

    Moar animations! + loader

--- app/index.js
+++ app/index.js
@@ -8,15 +8,59 @@ import 'styles/index.scss';
 // ================================
 // START YOUR APP HERE
 // ================================
-window.onload = () => {
 
-  let logo = document.querySelector('#logo')
-
-  window.addEventListener('click', evt => {
-    evt.preventDefault()
+class Loader {
+  constructor(){
+    this.loader = document.querySelector('section.loader')
+  }
+  enableLoader(){
+    this.loader.classList.add('show')
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('test');
+        this.loader.classList.remove('show');
+        document.body.classList.remove('no-animated')
+        resolve();
+      }, 2000);
+    });
+  }
+  enableLoaderAfter(){
+    this.loader.classList.add('show')
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('test');
+        this.loader.classList.remove('show');
+        document.body.classList.remove('no-animated')
+        resolve();
+      }, 2000);
+    });
+  }
+  toggleAppears(timing){
     document.querySelectorAll('.appear,.disappear').forEach(item => {
       item.classList.toggle('appear')
       item.classList.toggle('disappear')
+    })
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, timing);
+    });
+  }
+}
+
+window.onload = () => {
+
+  let logo = document.querySelector('#logo')
+  let loader = new Loader();
+  loader.enableLoader().then(() => {
+    loader.toggleAppears(0);
+  })
+  window.addEventListener('click', evt => {
+    evt.preventDefault()
+    loader.toggleAppears(1000).then(()=> {
+      loader.enableLoader().then(() => {
+    loader.toggleAppears(0);
+      })
     })
   })
 }
